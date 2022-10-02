4d2233632b41e2f055d86badf9c08155b75d3732
AbdullahAyan <75751013+AbdullahAyan@users.noreply.github.com>
2022-10-02 21:53:42 +0300

    almost done

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -10,111 +10,116 @@ import UIKit
 
 class OperationModel {
     static var sharedText = "0"
-    
-    var operationStack = [Double]()
-    
-    var currentText = OperationModel.sharedText {
+    var output: Double = 0 {
         didSet {
-            if currentText.contains(",") {
-                currentText = currentText.replacingOccurrences(of: ",", with: ".")
-            }
-            OperationModel.sharedText = currentText
+            OperationModel.sharedText = String(output)
         }
     }
-    
-    
-    var currentNumber: Double {
-        get {
-            if let number = Double(currentText) {
-                return number
-            }
-            currentText = String(self.currentNumber)
-            return 0.0
-        }
-        set {
-            if let int = Int(exactly: newValue) {
-                self.currentText = String(int)
-            }else {
-                self.currentText = String(newValue)
-            }
-        }
-    }
-    
-    
 }
 
 class OperationViewModel {
     var model = OperationModel()
-    var operationSymbol = ""
-    
-    
-    func initialText() -> String {
-        return model.currentText
-    }
-    
-    func write(number: String) -> String {
-        if model.currentText == "0" {
-            model.currentText = number
-            return number
+    var operationElemets = [Double]()
+    var previousSymbol = ""
+    var nextSembol = ""
+    var onWriting = "" {
+        didSet {
+            OperationModel.sharedText = onWriting
         }
-        model.currentText += number
-        return model.currentText
     }
     
-    func calculate(symbol: String) -> String {
+    func write(number : String) -> String {
+        if onWriting == "0" {
+            onWriting = number
+        }else if number == ","{
+            onWriting += "."
+        }else {
+            onWriting += number
+        }
+        return onWriting
+    }
+    
+    func operatorClicked(symbol: String) -> String {
+        if symbol != "=" {
+            nextSembol = symbol
+        }
+        operationElemets.append(Double(onWriting)!)
+        onWriting = "0"
+        
+        if operationElemets.count == 2 {
+            switch previousSymbol {
+            case "+":
+                model.output = (operationElemets.removeLast() + operationElemets.removeLast())
+                operationElemets.append(model.output)
+
+                previousSymbol = nextSembol
+            case "×":
+                model.output = (operationElemets.removeLast() * operationElemets.removeLast())
+                operationElemets.append(model.output)
+
+                previousSymbol = nextSembol
+            case "-":
+                let second = operationElemets.removeLast()
+                let first = operationElemets.removeFirst()
+                model.output = first - second
+                operationElemets.append(model.output)
+
+                previousSymbol = nextSembol
+            case "÷":
+                let second = operationElemets.removeLast()
+                let first = operationElemets.removeFirst()
+                model.output = first / second
+                operationElemets.append(model.output)
+
+                previousSymbol = nextSembol
+            case "=":
+                print(11)
+            default:
+                model.output = 0
+            }
+        }else {
+            previousSymbol = symbol
+        }
+        
+
+        
+        print(operationElemets)
+        
+        if let int = Int(exactly: operationElemets[0] ) {
+    
+            return String(int)
+        }else {
+
+            return String(operationElemets[0])
+        }
+    }
+    
+    func changerClicked(symbol: String) -> String {
         switch symbol {
-        case "AC" :
-            model.currentText = "0"
+        case "AC":
+            onWriting = "0"
+            operationElemets = [Double]()
         case "+/-":
-            if model.currentText.contains("-") {
-                model.currentText.removeFirst(1)
+            if onWriting.contains("-") {
+                onWriting.removeFirst(1)
             } else {
-                model.currentText = "-" + model.currentText
+                onWriting = "-" + onWriting
             }
         case "%":
-            model.currentNumber /= 100
-        case "+":
-            if model.operationStack.isEmpty {
-                model.operationStack.append(model.currentNumber)
-                model.currentNumber = 0
-                operationSymbol = "+"
-            } else {
-                model.operationStack.append(model.currentNumber + model.operationStack.removeLast())
-                model.currentText = String(model.operationStack.removeLast())
-                operationSymbol = "+"
-            }
-        case "-":
-            if model.operationStack.isEmpty {
-                model.operationStack.append(model.currentNumber)
-                model.currentNumber = 0
-                operationSymbol = "-"
-            } else {
-                model.operationStack.append(model.operationStack.removeLast() - model.currentNumber)
-                model.currentText = String(model.operationStack.removeLast())
-                operationSymbol = "-"
-            }
-        case "=":
-            if model.operationStack.isEmpty {
-                model.currentNumber = 0
-                print("girdi")
-            } else {
-                model.currentNumber = Double(calculate(symbol: operationSymbol))!
-                operationSymbol = ""
-
-            }
+            onWriting = String(Double(onWriting)! / 100)
         default:
-            return ""
+            onWriting = "0"
         }
-        print(model.operationStack)
-        return model.currentText
+        
+        return onWriting
     }
+    
 }
 
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var previousOperationLabel: UILabel!
-    @IBOutlet weak var clearButton: UIButton!
     
     var viewModel: OperationViewModel?
     
@@ -122,7 +127,6 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         viewModel = OperationViewModel()
-        displayLabel.text = viewModel?.initialText()
     }
     
     @IBAction func numberButtonClicked(_ sender: UIButton) {
@@ -130,7 +134,14 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func operationButtonClicked(_ sender: UIButton) {
-        displayLabel.text = viewModel?.calculate(symbol: (sender.titleLabel?.text)!)
+        displayLabel.text = viewModel?.operatorClicked(symbol: (sender.titleLabel?.text)!)
     }
+    
+    
+    @IBAction func changerClicked(_ sender: UIButton) {
+        displayLabel.text = viewModel?.changerClicked(symbol: (sender.titleLabel?.text)!)
+    }
+    
+    
 }
 
